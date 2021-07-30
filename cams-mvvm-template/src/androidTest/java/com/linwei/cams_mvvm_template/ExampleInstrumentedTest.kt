3c7289aae35551ee53284d91e96ad5f24c21eb50
linwei <linw@szwgmf.com>
2021-07-30 09:49:14 +0800

    修改配置信息

--- cams-mvvm-template/src/androidTest/java/com/linwei/cams_mvvm_template/ExampleInstrumentedTest.kt
+++ cams-mvvm-template/src/androidTest/java/com/linwei/cams_mvvm_template/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.linwei.wsgithubappkotlin", appContext.packageName)
+        assertEquals("com.linwei.cams_mvvm_template", appContext.packageName)
     }
 }
